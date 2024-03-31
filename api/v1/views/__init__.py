fc12a8aab94e05faa1577ad2f0d47eb031e1f270
balmed <iwymed@gamil.com>
2024-03-31 09:00:50 -0700

    task2m4

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""Initialize the views blueprint"""
-from flask import Blueprint
+"""
+Views for AirBnB_clone_v3
+"""
 
+from flask import Blueprint
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 from api.v1.views.index import *
@@ -11,4 +13,3 @@ from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
-from api.v1.views.places_amenities import *
