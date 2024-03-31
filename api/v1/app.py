11f898020a91bbcec6a74e0e21a15765e488cff0
balmed <iwymed@gamil.com>
2024-03-30 22:43:51 -0700

    task3m2

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,7 +5,7 @@ The RESTful api starts here. The api aids data access in the app.
 from os import getenv
 
 from flask import Flask, jsonify
-#from flask_cors import CORS
+from flask_cors import CORS
 
 from api.v1.views import app_views
 from models import storage
@@ -13,9 +13,7 @@ from models import storage
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
-#CORS(app, origins=["0.0.0.0"])
-host = getenv("HBNB_API_HOST", "0.0.0.0")
-port = getenv("HBNB_API_PORT", "5000")
+CORS(app, origins=["0.0.0.0"])
 
 
 @app.teardown_appcontext
@@ -30,4 +28,6 @@ def not_found(error):
 
 
 if __name__ == "__main__":
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = getenv("HBNB_API_PORT", "5000")
     app.run(host, port, threaded=True, debug=True)
